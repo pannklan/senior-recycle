89a8e4aac2c5d3638279a800200e23f8c400844d
pannklan <pklankasem@gmail.com>
2025-03-05 15:23:02 +0700

    idk what im doing

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+    res.send("Server is running! Use the API endpoints.");
+});
+
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("MongoDB connected"))
     .catch(err => console.error("MongoDB connection error:", err));
